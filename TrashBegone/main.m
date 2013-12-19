9bdfdbf4eec181f5d2e2cedd23d7c9ef070b862a
Alex Nichol <alexnichol@comcast.net>
2013-12-19 17:42:42 -0500

    Initial Commit

---
+++ TrashBegone/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TrashBegone
+//
+//  Created by Alex Nichol on 12/19/13.
+//  Copyright (c) 2013 Alex Nichol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
